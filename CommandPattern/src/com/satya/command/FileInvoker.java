136a85d5a1ea3e07cfc5b7734df6550ea6f54934
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-24 16:50:50 +0530

    Add files via upload

---
+++ CommandPattern/src/com/satya/command/FileInvoker.java
@@ -0,0 +1,17 @@
+package com.satya.command;
+
+
+public class FileInvoker {
+
+	public Command command;
+	
+	public FileInvoker(Command c){
+		this.command=c;
+	}
+	
+	public void execute(){
+		this.command.execute();
+	}
+}
+
+
---
+++ CommandPattern/src/com/satya/command/FileSystemReceiver.java
@@ -0,0 +1,9 @@
+package com.satya.command;
+
+public interface FileSystemReceiver {
+
+	void openFile();
+	void writeFile();
+	void closeFile();
+}
+
